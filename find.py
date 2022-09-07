2bb08303e67771e7891a79640687fff21a00a5ac
Aidan Semancik <903140@H2WHK18GQ6NW.tld>
2022-09-07 11:56:13 -0500

    couple updates but still struggling. taking a break and moving on to another project.

--- find.py
+++ find.py
@@ -1,22 +1,21 @@
 import random
 
-word = ["exhibition", "chocolate", "oil", "basin", "elbow", "dignity", "sow", "action", "producer", "reason", "identification", "brother", "pie", "raid", "figure", "joy", "flatware", "post", "automatic", "pocket", "fragment", "apparatus", "executive", "endorse", "deep", "pour", "case", "zone", "length", "root"]
+rand_word = ["exhibition", "chocolate", "oil", "basin", "elbow", "dignity", "sow", "action", "producer", "reason", "identification", "brother", "pie", "raid", "figure", "joy", "flatware", "post", "automatic", "pocket", "fragment", "apparatus", "executive", "endorse", "deep", "pour", "case", "zone", "length", "root"]
 
 hint = ["a display or demonstration of a particular skill.", "a sweet treat that originates from a seed.", "blank doesn't mix with water.", "British word for sink.", "where is your funny bone?", "a sense of pride in oneself; self-respect.", "plant the seeds of (a plant or crop)", "the process of doing something.", "oversees the creation of films.", "a cause, explanation, or justification for an action or event.", "a driver's license is an example of blank.", "a man or boy in relation to other sons and daughters of his parents.", "a cherry blank.", "a sudden attack.", "kids like to play with an action blank.", "synonym for happy.", "another word for utensils.", "something (such as a message) that is published online.", "working by itself with little or no direct human control.", "I often carry my phone in my blank.", "a small part of something.", "a complex structure within an organization or system.", "the person or branch of a government responsible for putting policies or laws into effect.", "declare one's public approval or support of.", "this water is way too blank!", "when I am thirsty I am going to blank a drink.", "an instance of a disease or problem.", "an area or stretch of land having a particular characteristic, purpose, or use, or subject to particular restrictions.", "the measurement of something from end to end.", "the part of the plant attached to the ground."]
 
 choice = random.randint(1, 30)
-correct_word = word[choice]
+word = rand_word[choice]
 print(hint[choice])
 
-hidden = []
+hidden = ''
 
-for x in correct_word:
+for x in word:
     hidden.append("-")
- 
-print(hidden)
-print(correct_word)
 
-attempts = (len(correct_word)) + 2
+print(word)
+
+attempts = (len(word)) + 2
 print("You have " + str(attempts) + " attempts.")
 
 for attempts in range(attempts):
@@ -26,14 +25,11 @@ for attempts in range(attempts):
         print("Please enter only one character at a time.")
         continue
 
-    if guess in correct_word:
+    if guess in word:
         print("Yay! " + guess + " is in the word!")
-        hidden.insert(correct_word.index(guess), guess)
+        hidden.insert(word.index(guess), guess)
 
-        # print(hidden[:1] + guess + hidden[1 + 1:])
-        
-
-        print(hidden)
+        print(hidden[:1] + guess + hidden[1 + 1:])
         
     else:
         print("Sorry, the word does not include: " + guess)
